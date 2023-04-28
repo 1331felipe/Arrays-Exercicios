24fc5ff4b1349d711103666b10c540b5b3de4819
1331felipe <felipehuh2023@gmail.com>
2023-04-28 20:59:35 -0300

    Arrays-Exercices

---
+++ index.js
@@ -0,0 +1,43 @@
+// const number = [12,15,18]
+// console.log (number[0]);
+
+// const dogNames = ["Bulldog","Golden","Rottweiler","German Shepherd","Puddle"]
+// let theName = dogNames[Number=prompt("type any number.")]
+
+// console.log (theName);
+
+// const harryPotterNames = ["harry","potter","harry styles"]
+// console.log(harryPotterNames.length)
+
+// const prettyNames = ["Felipe","Martins","Tyler"]
+// console.log (prettyNames.includes("Felipe"))
+// console.log (prettyNames.includes("Arthur"))
+
+// const randomNumbers = [4,5,6,7,8]
+// randomNumbers.push (9,10,11)
+// console.log (randomNumbers)
+
+// randomNumbers.pop (5,8,9)
+// console.log(randomNumbers)
+
+// randomNumbers.splice(3,5)
+// console.log (randomNumbers)
+
+// const values =[1,2,3,4,5,6]
+// console.log(values.length)
+
+// values.push(7)
+// values.splice(3,2)
+
+// console.log(values.length)
+
+
+
+
+
+
+
+
+
+
+
